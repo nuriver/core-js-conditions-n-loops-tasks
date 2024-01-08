748c9e26a1722e5276153362844103a1b018cbf9
Aleksei Golubkov <nuriver@gmail.com>
2024-01-08 18:36:14 +0100

    feat: solve isContainNumber task

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -237,8 +237,6 @@ function isPalindrome(str) {
   }
   return true;
 }
-isPalindrome('0123210');
-console.log(isPalindrome('0123210'));
 /**
  * Finds the first occurrence of a letter in a string.
  * In this task, the use of methods of the String and Array classes is not allowed.
@@ -277,10 +275,14 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  for (let i = 0; i < `${num}`.length; i += 1) {
+    if (`${num}`[i] === `${digit}`) {
+      return true;
+    }
+  }
+  return false;
 }
-
 /**
  * Finds the index of an element in an array where the sum of elements to the left equals the sum of elements to the right.
  * If such an index does not return -1.
